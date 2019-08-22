55c18b5e1b2ac28f2649d231afca8a9cc04e654d
zhoujiahao <zhou.jiahao@foxmail.com>
2019-08-22 21:06:36 +0800

    login work have done

---
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -0,0 +1,39 @@
+package com.mmall.controller.portal;
+
+import com.mmall.common.Const;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/user/")
+public class UserController {
+    //去找iUserService注解
+    @Autowired
+    private IUserService iUserService;
+    /**
+     * 用户登录
+     * @param username
+     * @param password
+     * @param session
+     * @return
+     * resonsebody为JSON自动转化注解
+     */
+    @RequestMapping(value = "login.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> login(String username, String password, HttpSession session){
+        //service->mybatis->dao
+        ServerResponse<User> response = iUserService.login(username,password);
+        if(response.isSuccess()){
+            session.setAttribute(Const.CURRENT_USER,response.getData());
+        }
+        return response;
+    }
+}
